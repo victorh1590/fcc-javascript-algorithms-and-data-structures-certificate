e6ffe41065f2f5d4506ea215ff8d50fc406bd2d1
Victor H. Faria <victorfaria1590@gmail.com>
2023-01-25 19:55:56 -0300

    Regenerated the code with target=es2022 and removed console.logs

--- 1_Palindrome_Checker/Palindrome_Checker.js
+++ 1_Palindrome_Checker/Palindrome_Checker.js
@@ -1,12 +1,9 @@
 function palindrome(str) {
-    var rx = /[\W\s_]/gi;
-    var text = str.trim().replace(rx, "").toLowerCase();
-    var middle = (text.length - 1) / 2;
-    console.log(text);
-    console.log(middle);
-    console.log(Math.floor(middle));
-    var i = 0;
-    var j = text.length - 1;
+    let rx = /[\W\s_]/gi;
+    let text = str.trim().replace(rx, "").toLowerCase();
+    let middle = (text.length - 1) / 2;
+    let i = 0;
+    let j = text.length - 1;
     while (i <= Math.floor(middle) && j >= Math.floor(middle + 1) && j != i) {
         if (text[i++] !== text[j--])
             return false;
--- 1_Palindrome_Checker/Palindrome_Checker.ts
+++ 1_Palindrome_Checker/Palindrome_Checker.ts
@@ -2,9 +2,6 @@ function palindrome(str: string): Boolean {
   let rx: RegExp = /[\W\s_]/gi;
   let text: string = str.trim().replace(rx, "").toLowerCase();
   let middle: number = (text.length - 1) / 2;
-  console.log(text);
-  console.log(middle);
-  console.log(Math.floor(middle));
   let i: number = 0; 
   let j: number = text.length - 1;
   while (i <= Math.floor(middle) && j >= Math.floor(middle + 1) && j != i) {
