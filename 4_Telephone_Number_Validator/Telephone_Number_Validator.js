ce97bd4a4f7c7ace7aff5d2de7acdb34a24d1d52
Victor H. Faria <victorfaria1590@gmail.com>
2023-01-26 03:35:22 -0300

    Project 4 Telephone Number Validator code.

---
+++ 4_Telephone_Number_Validator/Telephone_Number_Validator.js
@@ -0,0 +1,15 @@
+function telephoneCheck(str) {
+    let patterns = [
+        /^(\d{3}[ -]\d{3}[ -]\d{4}$)/i,
+        /^(\(\d{3}\) ?\d{3}-\d{4})$/i,
+        /^((1 )?\d{3}[ -]\d{3}[ -]\d{4})$/i,
+        /^(1 ?\(\d{3}\)[ -]?\d{3}[ -]\d{4})$/i,
+        /^(\d{10})$/i
+    ];
+    for (let i = 0; i < patterns.length; i++) {
+        if (patterns[i].test(str))
+            return true;
+    }
+    return false;
+}
+telephoneCheck("555-555-5555");
--- 4_Telephone_Number_Validator/Telephone_Number_Validator.ts
+++ 4_Telephone_Number_Validator/Telephone_Number_Validator.ts
@@ -1,5 +1,16 @@
-function telephoneCheck(str) {
-  return true;
+function telephoneCheck(str: string): boolean {
+  let patterns: RegExp[] = 
+  [
+    /^(\d{3}[ -]\d{3}[ -]\d{4}$)/i, 
+    /^(\(\d{3}\) ?\d{3}-\d{4})$/i,
+    /^((1 )?\d{3}[ -]\d{3}[ -]\d{4})$/i,
+    /^(1 ?\(\d{3}\)[ -]?\d{3}[ -]\d{4})$/i,
+    /^(\d{10})$/i
+  ];
+  for(let i = 0; i < patterns.length; i++) {
+    if(patterns[i].test(str)) return true;
+  }
+  return false;
 }
 
 telephoneCheck("555-555-5555");
